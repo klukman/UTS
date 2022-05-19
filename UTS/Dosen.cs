98a9b8d69b4d691decb1daaf22005fbb4ab1cdc1
klukman <lukman.arisandi@students.amikom.ac.id>
2022-05-19 13:39:59 +0700

    Add project files.

---
+++ UTS/Dosen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UTS_1521_Lukman
+{
+    public class Dosen
+    {
+        public int ID { get; set; }
+        public string Nama { get; set; }
+        public int NIK { get; set; }
+        public string Gender { get; set; }
+        public string Couser { get; set; }
+
+        
+    }
+}
---
+++ UTS/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace UTS_1521_Lukman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dosen dosen = new Dosen();
+
+            dosen.ID = 190302123;
+            dosen.Nama = "Kamarudin, M.Kom";
+            dosen.NIK = 987654;
+            dosen.Gender = "Laki-laki";
+            dosen.Couser = "Pemrograman";
+
+            Console.WriteLine("NO \tID \t\tNama \t\t\tNIK \tGender \t\tCouser");
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("1. \t{0} \t{1} \t{2} \t{3} \t{4} \n", dosen.ID, dosen.Nama, dosen.NIK, dosen.Gender, dosen.Couser );
+
+            Dosen dosen2 = new Dosen();
+
+            Console.WriteLine("Nama:");
+            dosen2.Nama = Console.ReadLine();
+            Console.WriteLine("NIK");
+            dosen2.NIK = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("NO \tID \t\tNama \t\tNIK \tGender \t\tCouser");
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("1. \t{0} \t{1} \t{2} \t{3} \t{4}", dosen.ID, dosen.Nama, dosen.NIK, dosen.Gender, dosen.Couser);
+            Console.WriteLine("2. \t0 \t\t{0} \t\t{1} \t0 \t\t0 \n", dosen2.Nama, dosen2.NIK);
+
+            Console.WriteLine("Couser:");
+            dosen2.Couser = Console.ReadLine();
+            Console.WriteLine("NO \tID \t\tNama \t\tNIK \tGender \t\tCouser");
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("1. \t{0} \t{1} \t{2} \t{3} \t{4}", dosen.ID, dosen.Nama, dosen.NIK, dosen.Gender, dosen.Couser);
+            Console.WriteLine("2. \t0 \t\t{0} \t\t{1} \t0 \t\t{2}", dosen2.Nama, dosen2.NIK, dosen2.Couser);
+
+            
+            Console.ReadKey();
+        }
+    }
+}
+
